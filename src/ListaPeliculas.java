a39a2a2144093ff82255c784da3f3f6581682d13
riki <riki@RIKI-Debian>
2017-08-05 11:23:35 -0500

    lista peliculas añadido y comentado

---
+++ src/ListaPeliculas.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+/**
+ * @author ANAHI NARVAEZ, RICARDO RANGLES, RODRIGO SUAREZ
+ * Clase Pelicula la cual crea objetos del tipo Pelicula.
+ */
+public class ListaPeliculas {
+
+	private ArrayList<Pelicula> listaPeliculas;
+
+	/**
+	 * Constructor 
+	 */
+	public ListaPeliculas() {
+
+		this.listaPeliculas=new ArrayList<Pelicula>();
+	}
+
+	/**
+	 * @param vpelicula lista de peliculas
+	 */
+	public void ingresarpelicula(Pelicula vpelicula){
+		
+		listaPeliculas.add(vpelicula);
+	}
+
+	/**
+	 * Muestra la cartelera de peliculas
+	 */
+	public void cartelera(){
+		
+		for(Pelicula vpelicula:listaPeliculas){
+			System.out.println(vpelicula+"\n");
+		}
+	}
+
+	/**
+	 * Muestra la informacion de la sala
+	 */
+	public void informacionSala(){
+
+		for(Pelicula vpelicula:listaPeliculas){
+			
+			System.out.println(vpelicula.getNombre());
+			vpelicula.salas();
+		}
+	}
+}
