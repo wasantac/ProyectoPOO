7828dea3782bf0a112056490117959fc129df8ea
Patricio Rangles <patricio.rangles@trescloud.com>
2017-08-06 18:05:05 -0500

    modificado horarios

--- src/ListadoHorarioSala.java
+++ src/ListadoHorarioSala.java
@@ -55,11 +55,11 @@ public class ListadoHorarioSala implements Serializable {
 	}
 
 
-	public void ImprimirHorarioPelicula(Pelicula pelicula, int anio, Month august, int dia){
+	public void ImprimirHorarioPelicula(Pelicula pelicula, int anio, int mes, int dia){
 
 		System.out.println(pelicula.getNombre());
 
-		System.out.println(String.valueOf(anio)+" / "+august+" / "+String.valueOf(dia));
+		System.out.println(String.valueOf(anio)+" / "+mes+" / "+String.valueOf(dia));
 
 		int i= 0;
 
@@ -67,21 +67,16 @@ public class ListadoHorarioSala implements Serializable {
 
 			if(hs.getInicio().getDayOfMonth() == dia){
 
-				System.out.println("Sala "+hs.getSala().getNumSala()+": "+ String.valueOf(hs.getInicio().getHour()+":"+hs.getInicio().getMinute()+" a "+hs.getFin().getHour()+":"+hs.getFin().getMinute()));
-
+				System.out.println(Integer.toString(i + 1) + ") Sala " + hs.getSala().getNumSala() + ", Formato " + hs.getSala().getFormato() + ": de" + String.valueOf(hs.getInicio().getHour() +":"+hs.getInicio().getMinute()+" a "+hs.getFin().getHour()+":"+hs.getFin().getMinute()));
 				i++;
 
 			}
 		}	
 
-		if(i!=0){
-
-			System.out.println("Se encontraron al menos 1 resultado");
-
-		}else{
+		if(i==0){
 
 			System.out.println("No se encontraron resultados");
-
+		
 		}
 	}
 
