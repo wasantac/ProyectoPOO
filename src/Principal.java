a01b858fb705c5532fd388f5b47672814354e533
riki <riki@RIKI-Debian>
2017-08-06 21:55:35 -0500

    principal

--- src/Principal.java
+++ src/Principal.java
@@ -16,7 +16,6 @@ public class Principal implements Serializable  {
 	static ArrayList<Factura> facturas = new ArrayList<Factura>();
 	static ListaPeliculas peliculas = new ListaPeliculas();
 	static ListadoHorarioSala horarios = new ListadoHorarioSala();
-	static ListaFormatosSala formatos = new ListaFormatosSala(); 
 
 	public static void main(String[] args) throws Exception {
 
@@ -212,120 +211,99 @@ public class Principal implements Serializable  {
  */
 
 
-	private static void CrearFicheroInicial(String sFichero) {
-		// Lectura del archivo
+private static void CrearFicheroInicial(String sFichero) {
+	// Lectura del archivo
 
-		// Creacion de las 4 salas
-		Sala sala1 = new Sala();
-		sala1.setNumSala(1);
-		Sala sala2 = new Sala();
-		sala2.setNumSala(2);
-		Sala sala3 = new Sala();
-		sala3.setNumSala(3);
-		Sala sala4 = new Sala();
-		sala4.setNumSala(4);
+	// Creacion de las 4 salas
+	Sala sala = new Sala();
+	sala.setNumSala(1);
 
-		ListaAsientos asientosSala1 = new ListaAsientos();
-		asientosSala1.setColumnas(9);
-		asientosSala1.setFilas(12);
+	ListaAsientos asientosSala1 = new ListaAsientos();
+	asientosSala1.setColumnas(9);
+	asientosSala1.setFilas(12);
 
-		ListaAsientos asientosSala2 = new ListaAsientos();
-		asientosSala2.setColumnas(8);
-		asientosSala2.setFilas(10);
+	ListaFormatosSala formatos = new ListaFormatosSala(); 
 
-		ListaAsientos asientosSala3 = new ListaAsientos();
-		asientosSala3.setColumnas(5);
-		asientosSala3.setFilas(7);
+	Asiento asiento1 = new Asiento('h',3);
 
-
-		Asiento asiento1 = new Asiento('h',3);
-	    Asiento asiento2 = new Asiento('b',5);
-		Asiento asiento3 = new Asiento('d',1);
-		Asiento asiento4 = new Asiento('f',7);
-
-		
-		FormatoSala formato = new FormatoSala();
-		formatos.AgregarFormato(formato);
-		FormatoSala formato1 = new FormatoSala("3D", 7.50);
-		formatos.AgregarFormato(formato1);
-		FormatoSala formato2 = new FormatoSala("IMAX", 9.25);
-		formatos.AgregarFormato(formato2);
-		
-		sala1.setAsientos(asientosSala1);
-		sala1.setFormato(formato);
-
-		Pelicula pelicula1 = new Pelicula("TRANSFORMERS", 120, "Ingles", "12");
-		Pelicula pelicula2 = new Pelicula("AVENGERS", 180, "Espanol", "Todo Publico");
-		Pelicula pelicula3 = new Pelicula("IRON MAN 3", 190, "Ingles", "Todo Publico");
-		Pelicula pelicula4 = new Pelicula("MI NOVIA POLY", 160, "Ingles", "12");
-		
-		peliculas.ingresarpelicula(pelicula1);	
-		peliculas.ingresarpelicula(pelicula2);	
-		peliculas.ingresarpelicula(pelicula3);	
-		peliculas.ingresarpelicula(pelicula4);	
-
-		LocalDateTime inicio = LocalDateTime.of(2017, Month.AUGUST, 5, 9, 10, 0);
-		LocalDateTime fin = LocalDateTime.of(2017, Month.AUGUST, 5, 11, 00, 0);
-		HorarioSala horario1 = new HorarioSala(inicio, fin, pelicula1, sala1);
-
-		LocalDateTime inicio1 = LocalDateTime.of(2017, Month.AUGUST, 5, 9, 10, 0);
-		LocalDateTime fin1 = LocalDateTime.of(2017, Month.AUGUST, 5, 11, 10, 0);
-		LocalDateTime inicio2 = LocalDateTime.of(2017, Month.AUGUST, 6, 12, 10, 0);
-		LocalDateTime fin2 = LocalDateTime.of(2017, Month.AUGUST, 6, 13, 30, 0);
-		LocalDateTime inicio3 = LocalDateTime.of(2017, Month.AUGUST, 7, 20, 10, 0);
-		LocalDateTime fin3 = LocalDateTime.of(2017, Month.AUGUST, 7, 11, 22, 10);
-
-		horarios.AgregarHorario(horario1);
-		horarios.AgregarHorario(inicio1, fin1, pelicula1, sala2);
-		horarios.AgregarHorario(inicio2, fin2, pelicula1, sala3);
-		horarios.AgregarHorario(inicio3, fin3, pelicula2, sala1);
-
-
-		horarios.ImprimirHorarioPelicula(pelicula1, 2017, 8, 5); 
-		horarios.ImprimirHorarioPelicula(pelicula1, 2017, 8, 6);
-		horarios.ImprimirHorarioPelicula(pelicula2, 2017, 8, 7);
-		horarios.ImprimirHorarioPelicula(pelicula3, 2017, 8, 8);
-
-
-		Empleado vendedor1 = new Empleado("Patricio","Rangles","1717104127");
-		vendedor1.setPosicion("Vendedor");
-
-		Cliente cliente1 = new Cliente("Luchito Cifuentes","1823632929");
-		cliente1.setTarjeta(1);
-
-		Factura factura = new Factura();
-		factura.setCliente(cliente1);
-		factura.setFecha(LocalDateTime.of(2017, Month.AUGUST, 5, 07, 33, 30));
-
-		ArrayList<Asiento> asientosFactura = new ArrayList<Asiento>(); 
-		asientosFactura.add(asiento1);
-
-		LineaFactura lineafactura = new LineaFactura();
-		lineafactura.setCantidad(3);
-		lineafactura.setPrecioUnitario(sala1.getFormato().getPrecio(), cliente1.getTarjeta());
-		lineafactura.setAsientos(asientosFactura);
-		lineafactura.setHorarioSala(horario1);
-
-		factura.AgregarLineaFactura(lineafactura);
-
-		///////////////////////////////////////////////////////////////////////////////
-		//pruebas clientes
-		peliculas.Cartelera();
-		formatos.ObtenerPrecio(cliente1.getTarjeta(), "2D");
-		// desde escoger pelicula hasta comprar boleto se hace con los objetos existentes con el ejemplo anterior
-
-
-
-		//// Escritura del archivo
-		//FileManager.WriteObject obj = new WriteObject();
-
-		//Address address = new Address();
-		//address.setStreet("wall street");
-		//address.setCountry("united state");
-
-		//obj.serializeAddress(address);
-
+	if (!asientosSala1.AsignarAsiento(asiento1)) {
+		System.out.println("ya existe el asiento");
 	}
+
+	//asientosSala1.RemoverAsiento('h', 3);
+	sala.setAsientos(asientosSala1);
+	FormatoSala formato = new FormatoSala();
+	sala.setFormato(formato);
+	formatos.AgregarFormato(formato);
+
+	Pelicula pelicula = new Pelicula();
+	pelicula.setNombre("TRANSFORMERS");
+
+	peliculas.ingresarpelicula(pelicula);	
+
+	LocalDateTime inicio = LocalDateTime.of(2017, Month.AUGUST, 5, 9, 10, 0);
+	LocalDateTime fin = LocalDateTime.of(2017, Month.AUGUST, 5, 11, 00, 0);
+	HorarioSala horario1 = new HorarioSala(inicio, fin, pelicula, sala);
+	horarios.AgregarHorario(horario1);
+
+	LocalDateTime inicio1 = LocalDateTime.of(2017, Month.AUGUST, 5, 9, 10, 0);
+	LocalDateTime fin1 = LocalDateTime.of(2017, Month.AUGUST, 5, 11, 10, 0);
+	LocalDateTime inicio2 = LocalDateTime.of(2017, Month.AUGUST, 6, 12, 10, 0);
+	LocalDateTime fin2 = LocalDateTime.of(2017, Month.AUGUST, 6, 13, 30, 0);
+	LocalDateTime inicio3 = LocalDateTime.of(2017, Month.AUGUST, 7, 20, 10, 0);
+	LocalDateTime fin3 = LocalDateTime.of(2017, Month.AUGUST, 7, 11, 22, 10);
+
+
+	horarios.AgregarHorario(inicio1, fin1, pelicula, sala);
+	horarios.AgregarHorario(inicio2, fin2, pelicula, sala);
+	horarios.AgregarHorario(inicio3, fin3, pelicula, sala);
+
+
+	horarios.ImprimirHorarioPelicula(pelicula, 2017, 8, 5); 
+	horarios.ImprimirHorarioPelicula(pelicula, 2017, 8, 6);
+	horarios.ImprimirHorarioPelicula(pelicula, 2017, 8, 7);
+	horarios.ImprimirHorarioPelicula(pelicula, 2017, 8, 8);
+
+
+	Empleado vendedor1 = new Empleado("Patricio","Rangles","1717104127");
+	vendedor1.setPosicion("Vendedor");
+
+	Cliente cliente1 = new Cliente("Luchito Cifuentes","1823632929");
+	cliente1.setTarjeta(1);
+
+	Factura factura = new Factura();
+	factura.setCliente(cliente1);
+	factura.setFecha(LocalDateTime.of(2017, Month.AUGUST, 5, 07, 33, 30));
+
+	ArrayList<Asiento> asientosFactura = new ArrayList<Asiento>(); 
+	asientosFactura.add(asiento1);
+
+	LineaFactura lineafactura = new LineaFactura();
+	lineafactura.setCantidad(3);
+	lineafactura.setPrecioUnitario(sala.getFormato().getPrecio(), cliente1.getTarjeta());
+	lineafactura.setAsientos(asientosFactura);
+	lineafactura.setHorarioSala(horario1);
+
+	factura.AgregarLineaFactura(lineafactura);
+
+	///////////////////////////////////////////////////////////////////////////////
+	//pruebas clientes
+	peliculas.Cartelera();
+	formatos.ObtenerPrecio(cliente1.getTarjeta(), "2D");
+	// desde escoger pelicula hasta comprar boleto se hace con los objetos existentes con el ejemplo anterior
+
+
+
+	//// Escritura del archivo
+	//FileManager.WriteObject obj = new WriteObject();
+
+	//Address address = new Address();
+	//address.setStreet("wall street");
+	//address.setCountry("united state");
+
+	//obj.serializeAddress(address);
+
+}
 
 //	public void serializeAddress(Address address) {
 //
@@ -378,5 +356,277 @@ public class Principal implements Serializable  {
 //		}
 //
 //	}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import java.io.File;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+
+public class Principal implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	static ArrayList<Sala> salas = new ArrayList<Sala>();
+	static ArrayList<Factura> facturas = new ArrayList<Factura>();
+	static ListaPeliculas peliculas = new ListaPeliculas();
+	static ListadoHorarioSala horarios = new ListadoHorarioSala();
+	
+	public static void main(String[] args) {
+
+	    File fichero = new File("data.txt");
+	    
+	    if (!fichero.exists()) {
+	    	CrearFicheroInicial("data.txt");	
+	    }
+	    //else {
+	    //	leerFicheroInicial(sFichero);
+	    //}
+
+	}
+	    
+	private static void CrearFicheroInicial(String sFichero) {
+		// Lectura del archivo
+
+		// Creacion de las 4 salas
+		Sala sala = new Sala();
+		sala.setNumSala(1);
+
+		ListaAsientos asientosSala1 = new ListaAsientos();
+		asientosSala1.setColumnas(9);
+		asientosSala1.setFilas(12);
+
+		ListaFormatosSala formatos = new ListaFormatosSala(); 
+
+		Asiento asiento1 = new Asiento('h',3);
+
+		if (!asientosSala1.AsignarAsiento(asiento1)) {
+			System.out.println("ya existe el asiento");
+		}
+
+		//asientosSala1.RemoverAsiento('h', 3);
+		sala.setAsientos(asientosSala1);
+		FormatoSala formato = new FormatoSala();
+		sala.setFormato(formato);
+		formatos.AgregarFormato(formato);
+		
+		Pelicula pelicula = new Pelicula();
+		pelicula.setNombre("TRANSFORMERS");
+        pelicula.setIdioma("Ingles");
+        pelicula.setCensura("Mayores de 12 años");
+		peliculas.ingresarpelicula(pelicula);
+
+		Scanner sc = new Scanner(System.in);
+		FileWriter fichero1 = null;
+		PrintWriter pw = null;
+		try{
+			fichero1 = new FileWriter("C:\\Users\\ANAHI\\Documents\\CrearDirectorio\\Peliculas.txt", true);
+			pw = new PrintWriter (fichero1);
+			pw.println(pelicula);		
+			}
+		catch(Exception e ){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(null != fichero1);
+				fichero1.close();
+			}catch(Exception e2){
+				e2.printStackTrace();
+			}
+		}
+		
+		
+		LocalDateTime inicio = LocalDateTime.of(2017, Month.AUGUST, 5, 9, 10, 0);
+		LocalDateTime fin = LocalDateTime.of(2017, Month.AUGUST, 5, 11, 00, 0);
+		HorarioSala horario1 = new HorarioSala(inicio, fin, pelicula, sala);
+		horarios.AgregarHorario(horario1);
+
+		LocalDateTime inicio1 = LocalDateTime.of(2017, Month.AUGUST, 5, 9, 10, 0);
+		LocalDateTime fin1 = LocalDateTime.of(2017, Month.AUGUST, 5, 11, 10, 0);
+		LocalDateTime inicio2 = LocalDateTime.of(2017, Month.AUGUST, 6, 12, 10, 0);
+		LocalDateTime fin2 = LocalDateTime.of(2017, Month.AUGUST, 6, 13, 30, 0);
+		LocalDateTime inicio3 = LocalDateTime.of(2017, Month.AUGUST, 7, 20, 10, 0);
+		LocalDateTime fin3 = LocalDateTime.of(2017, Month.AUGUST, 7, 11, 22, 10);
+
+
+		horarios.AgregarHorario(inicio1, fin1, pelicula, sala);
+		horarios.AgregarHorario(inicio2, fin2, pelicula, sala);
+		horarios.AgregarHorario(inicio3, fin3, pelicula, sala);
+
+
+		horarios.ImprimirHorarioPelicula(pelicula, 2017, Month.AUGUST, 5); 
+		horarios.ImprimirHorarioPelicula(pelicula, 2017, Month.AUGUST, 6);
+		horarios.ImprimirHorarioPelicula(pelicula, 2017, Month.AUGUST, 7);
+		horarios.ImprimirHorarioPelicula(pelicula, 2017, Month.AUGUST, 8);
+
+
+		Empleado vendedor1 = new Empleado("Patricio","Rangles","1717104127");
+		vendedor1.setPosicion("Vendedor");
+
+		Cliente cliente1 = new Cliente("Luchito Cifuentes","1823632929");
+		cliente1.setTarjeta(1);
+
+		Factura factura = new Factura();
+		factura.setCliente(cliente1);
+		factura.setFecha(LocalDateTime.of(2017, Month.AUGUST, 5, 07, 33, 30));
+		
+
+		ArrayList<Asiento> asientosFactura = new ArrayList<Asiento>(); 
+		asientosFactura.add(asiento1);
+
+		LineaFactura lineafactura = new LineaFactura();
+		lineafactura.setCantidad(3);
+		lineafactura.setPrecioUnitario(sala.getFormato().getPrecio(), cliente1.getTarjeta());
+		lineafactura.setAsientos(asientosFactura);
+		lineafactura.setHorarioSala(horario1);
+
+		factura.AgregarLineaFactura(lineafactura);
+		
+        FileWriter fichero2 = null;
+		
+		try{
+			fichero2 = new FileWriter("C:\\Users\\ANAHI\\Documents\\CrearDirectorio\\Facturas.txt", true);
+			pw = new PrintWriter (fichero2);
+			
+			pw.println("***********************");
+			pw.println(factura.getClass());
+			pw.println("Fecha: " + factura.getFecha());
+			pw.println(factura.getCliente());
+			pw.println("Horario de la pelicula: "+ lineafactura.getHorarioSala());
+			pw.println("Pelicula: " + pelicula.toString());
+			pw.println("Detalles de los Asientos: " + lineafactura.getAsientos());
+			pw.println("Numero de boletos: " + lineafactura.getCantidad());
+			pw.println("Precio unitario: " + lineafactura.getPrecioUnitario());
+			pw.println("TOTAL: " + factura.getPrecioTotal()+ "dolares.");
+			}
+		catch(Exception e ){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(null != fichero2);
+				fichero2.close();
+			}catch(Exception e2){
+				e2.printStackTrace();
+			}
+		}
+		
+		///////////////////////////////////////////////////////////////////////////////
+		//pruebas clientes
+		peliculas.Cartelera();
+		formatos.ObtenerPrecio(cliente1.getTarjeta(), "2D");
+		// desde escoger pelicula hasta comprar boleto se hace con los objetos existentes con el ejemplo anterior
+		asientosSala1.VisualizacionAsientos();
+
+		
+		//// Escritura del archivo
+		//FileManager.WriteObject obj = new WriteObject();
+
+		//Address address = new Address();
+		//address.setStreet("wall street");
+		//address.setCountry("united state");
+
+		//obj.serializeAddress(address);
+		
+	}
+	
+//	public void serializeAddress(Address address) {
+//
+//		FileOutputStream fout = null;
+//		ObjectOutputStream oos = null;
+//
+//		try {
+//
+//			fout = new FileOutputStream("c:\\temp\\address.ser");
+//			oos = new ObjectOutputStream(fout);
+//			oos.writeObject(address);
+//
+//			System.out.println("Done");
+//
+//		} catch (Exception ex) {
+//
+//			ex.printStackTrace();
+//
+//		} finally {
+//
+//			if (fout != null) {
+//				try {
+//					fout.close();
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//
+//			if (oos != null) {
+//				try {
+//					oos.close();
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//
+//		}
+//	}
+//
+//	public void serializeAddressJDK7(Address address) {
+//
+//		try (ObjectOutputStream oos =
+//				new ObjectOutputStream(new FileOutputStream("c:\\temp\\address2.ser"))) {
+//
+//			oos.writeObject(address);
+//			System.out.println("Done");
+//
+//		} catch (Exception ex) {
+//			ex.printStackTrace();
+//		}
+//
+//	}	
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
